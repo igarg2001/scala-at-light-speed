de0ec5f4b7d5b4a8d3e364af585a5319a807394e
Ishan Garg <igarg2001@gmail.com>
2023-06-23 12:11:57 +0530

    pattern matching

---
+++ src/main/scala/com/rockthejvm/PatternMatching.scala
@@ -0,0 +1,42 @@
+package com.rockthejvm
+
+object PatternMatching extends App {
+  val anInt = 20
+  val order = anInt match {
+    case 1 => "first"
+    case 2 => "second"
+    case 3 => "third"
+    case _ => anInt + "th"
+  }
+  //Pattern matching is an expression
+  println(order)
+
+  //case class decomposition
+  case class Person (name: String, age: Int)
+  val bob = Person("Bob", 27)
+
+  val greeting = bob match {
+    case Person(name, age) => s"Hi my name is $name and I am $age years old"
+    case _ => "I am not a person so I cannot greet you :("
+  }
+  println(greeting)
+
+  //deconstructing tuples
+  val favorite = ("Sebastian Vettel", "Ferrari")
+  val intro = favorite match {
+    case (driver, team) => s"$driver drives for team $team"
+    case _ => "I don't know what you're talking about"
+  }
+  println(intro)
+
+  //deconstructing lists
+  val aList = List (1, 2, 4)
+  val aMatch = aList match {
+    case List (_, 2, _) => "List contains 2 in index 1"
+    case _ => "Unknown List"
+  }
+  println(aMatch)
+
+  //cases are processed in sequence
+
+}
